d529331811f88780914dc3834b68c4a114ddfd0d
Moulali Syed <syedmoulali1997@gmail.com>
2023-10-14 09:48:45 +0530

    initial setup

---
+++ blog_backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const PORT = 8000;
+
+require('./db');
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Demo API is working' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
